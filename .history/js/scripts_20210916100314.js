f071a1aecd822dbe8e512d665399875e734826c9
= <=>
2021-09-16 10:04:01 +0700

    update index phan js nvabar

--- .history/js/scripts_20210916100314.js
+++ .history/js/scripts_20210916100314.js
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', event => {
         if (window.scrollY === 0) {
             navbarCollapsible.classList.remove('navbar-shrink')
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+            navbarCollapsible.classList.add('navbar-shrin')
         }
 
     };
